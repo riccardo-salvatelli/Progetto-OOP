d6c3b082a82a5a9874ccd9bc722e09a2d8597969
AlessandroMuscatello <S1089288@studenti.univpm.it>
2020-07-02 11:02:43 +0200

    Aggiunta parametri a Immagine e Testo

--- ProgettoOOP/src/main/java/it/progettoDecrypterApplication/java/model/Immagine.java
+++ ProgettoOOP/src/main/java/it/progettoDecrypterApplication/java/model/Immagine.java
@@ -1,19 +1,23 @@
 package it.progettoDecrypterApplication.java.model;
+
 import it.progettoOOP.DropboxDecrypter.exception.NumPixelException;
 
 public class Immagine extends File {
-	
-	private int numPixel;
 
-	public Immagine(int numPixel) {
+	private int numPixel;
+	private int[] dimImmagine = new int[2];
+
+	public Immagine(int numPixel, int[] dimImmagine) {
 		super();
 		this.numPixel = numPixel;
+		this.dimImmagine = dimImmagine;
 	}
+
 	public Immagine() {
 		super();
 		this.numPixel = -1;
 	}
-	
+
 	public int getNumPixel() throws NumPixelException {
 		if (numPixel >= 0)
 			return numPixel;
@@ -24,7 +28,13 @@ public class Immagine extends File {
 	public void setNumPixel(int numPixel) {
 		this.numPixel = numPixel;
 	}
-	
-	
+
+	public int[] getDimImmagine() {
+		return dimImmagine;
+	}
+
+	public void setDimImmagine(int[] dimImmagine) {
+		this.dimImmagine = dimImmagine;
+	}
 
 }
--- ProgettoOOP/src/main/java/it/progettoDecrypterApplication/java/model/Testo.java
+++ ProgettoOOP/src/main/java/it/progettoDecrypterApplication/java/model/Testo.java
@@ -3,26 +3,49 @@ package it.progettoDecrypterApplication.java.model;
 import it.progettoOOP.DropboxDecrypter.exception.NumCaratteriException;
 
 public class Testo extends File {
-	
+
 	private int numCaratteri;
-	public Testo(int numCaratteri) {
+	private int numParole;
+	private int numFrasi;
+
+	public Testo(int numCaratteri, int numParole, int numFrasi) {
 		super();
-		this.numCaratteri=numCaratteri;
+		this.numCaratteri = numCaratteri;
+		this.numParole = numParole;
+		this.numFrasi = numFrasi;
 		// TODO Auto-generated constructor stub
 	}
+
 	public Testo() {
 		super();
 		this.numCaratteri = -1;
 	}
+
 	public int getNumCaratteri() throws NumCaratteriException {
-		if (numCaratteri>=0)
+		if (numCaratteri >= 0)
 			return numCaratteri;
-		else 
+		else
 			throw new NumCaratteriException();
 	}
+
 	public void setNumCaratteri(int numCaratteri) {
 		this.numCaratteri = numCaratteri;
 	}
 
-	
+	public int getNumParole() {
+		return numParole;
+	}
+
+	public void setNumParole(int numParole) {
+		this.numParole = numParole;
+	}
+
+	public int getNumFrasi() {
+		return numFrasi;
+	}
+
+	public void setNumFrasi(int numFrasi) {
+		this.numFrasi = numFrasi;
+	}
+
 }
