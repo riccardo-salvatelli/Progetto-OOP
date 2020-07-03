5802d78df410a798b222ac91604d9362e716f81d
AlessandroMuscatello <S1089288@studenti.univpm.it>
2020-07-03 11:40:19 +0200

    Fix App e Controller

--- ProgettoOOP/src/main/java/it/progettoOOP/controller/DropboxDecrypterController.java
+++ ProgettoOOP/src/main/java/it/progettoOOP/controller/DropboxDecrypterController.java
@@ -1,20 +1,24 @@
 package it.progettoOOP.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.progettoOOP.model.File;
 
 @RestController
-public class Controller {
+public class DropboxDecrypterController {
 	
-	@GetMapping ("/esempio")
-	
-	public File provaget(@RequestParam(name="param1", defaultValue="World") String param1) {
-	return new File ("CIAO");
-		// TODO Auto-generated constructor stub
+	@GetMapping("/esempio")
+	public File esempio(@RequestParam(name="param1")String param1) {
+		return new File(param1);
+	}
+	@GetMapping("/esempio2")
+	public File esempio() {
+		return new File();
 	}
 	@PostMapping("/esempio")
 	public File exampleMethod2(@RequestBody String body) {
