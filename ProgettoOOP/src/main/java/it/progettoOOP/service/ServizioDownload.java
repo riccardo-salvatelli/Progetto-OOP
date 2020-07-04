293e5c22dfb5abe4845bfd96fdf8f9e647039632
AlessandroMuscatello <S1089288@studenti.univpm.it>
2020-07-04 12:25:25 +0200

    Aggiunta classe ServizioDownload

--- ProgettoOOP/src/main/java/it/progettoOOP/service/ServizioDownload.java
+++ ProgettoOOP/src/main/java/it/progettoOOP/service/ServizioDownload.java
@@ -1,16 +1,49 @@
 package it.progettoOOP.service;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 public class ServizioDownload {
-	public  post() {
 	
-	URL url = new URL("https://api.dropboxapi.com/2/files/list_folder");
-	URLConnection con = url.openConnection();
-	HttpURLConnection http = (HttpURLConnection)con;
-	http.setRequestMethod("POST"); // PUT is another valid option
-	http.setDoOutput(true);
-}
+	public void download() {
+	RestTemplate restTemplate = new RestTemplate();
+
+	String token =  "TOKEN";
+	String url = "https://content.dropboxapi.com/2/files/download";
+	String requestJson = "{\"path\":\"\"}";
+	
+	HttpHeaders headers = new HttpHeaders();
+	headers.setContentType(MediaType.APPLICATION_JSON);
+	headers.set("Authorization", token);
+	headers.set("Dropbox-API-Arg", "{\"path\": \"/Homework/math/Prime_Numbers.txt\"");
+	
+	HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
+	String answer = restTemplate.postForObject(url, entity, String.class);
+	System.out.println(answer);
+	
+	
+	/*/ send request and parse result
+	ResponseEntity<String> loginResponse = restTemplate
+			  .exchange(urlString, HttpMethod.POST, entity, String.class);
+			if (loginResponse.getStatusCode() == HttpStatus.OK) {
+			  JSONObject userJson = new JSONObject(loginResponse.getBody());
+			} else if (loginResponse.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+			  // nono... bad credentials
+			}
+	*/
+	}
+	
+
+			
+			
+		
+		
+		
+		
+		
+		
+	}
 }
